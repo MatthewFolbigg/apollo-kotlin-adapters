bc79ee33851c31611d530d925ca222ad7f3e40b5
Martin Bonnin <martin@mbonnin.net>
2025-06-26 14:52:55 +0200

    update to 0.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,7 @@ import com.gradleup.librarian.gradle.Librarian
 
 plugins {
   id("org.jetbrains.kotlin.jvm").version("2.0.0").apply(false)
+  id("com.gradleup.librarian").version("0.0.9").apply(false)
 }
 
-buildscript {
-  repositories {
-    mavenCentral()
-  }
-  dependencies {
-    classpath("com.gradleup.librarian:librarian-gradle-plugin:0.0.8")
-  }
-}
 Librarian.root(project)
