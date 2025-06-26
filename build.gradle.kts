58b97b3340f9d4811ab3a8f0179d9507c2c5babc
Martin Bonnin <martin@mbonnin.net>
2025-06-26 15:04:57 +0200

    Update librarian to 0.0.9 and switch publication to the Central Portal (#33)
    * update librarian
    
    * update to 0.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.gradleup.librarian.gradle.Librarian
 
 plugins {
   id("org.jetbrains.kotlin.jvm").version("2.0.0").apply(false)
-  id("com.gradleup.librarian").version("0.0.7").apply(false)
+  id("com.gradleup.librarian").version("0.0.9").apply(false)
 }
 
 Librarian.root(project)
